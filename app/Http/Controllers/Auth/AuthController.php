38a229894fa5bd0acee28df2143d05b0001bce41
Grace Kyalo <kyalograce21@gmail.com>
2025-04-30 00:21:47 +0300

    removed user abilities from login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -110,7 +110,7 @@ class AuthController extends Controller
         return response()->json([
             'token' => $token,
             'user' => $user,
-            'abilities' => $user->abilities(),
+            // 'abilities' => $user->abilities(),
         ]);
     }
 
