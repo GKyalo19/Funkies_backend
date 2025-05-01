e1eec17723d7514832ec89574d8835749980238e
Grace Kyalo <kyalograce21@gmail.com>
2025-05-01 12:24:05 +0300

    Update the update user method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -57,31 +57,45 @@ class UserController extends Controller
         return response()->json($userData);
     }
 
-    public function update(Request $request, User $user): JsonResponse
+    public function update(Request $request, $id)
     {
+        $userToUpdate = User::findOrFail($id);
+
         // $this->authorize('update', $user);
         $validated = $request->validate([
             'name' => ['sometimes', 'string', 'max:255'],
-            'email' => ['sometimes', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+            'email' => ['sometimes', 'string', 'email', 'max:255'],
             'password' => ['sometimes', 'string', 'min:8'],
-            // 'user_photo' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
-            // 'role_id' => ['sometimes', 'exists:roles,id'],
-            'is_active' => ['sometimes', 'boolean'],
         ]);
 
-        // if ($request->hasFile('user_photo')) {
-        //     $filename = $request->file('user_photo')->store('posts', 'public');
-        // } else {
-        //     $filename = Null;
-        // }
-        // $user->user_photo = $filename;
-
-        if (isset($validated['password'])) {
-            $validated['password'] = Hash::make($validated['password']);
+        if($userToUpdate){
+            $userToUpdate->name = $validated['name'];
+            $userToUpdate->email = $validated['email'];
+            $userToUpdate->password = $validated['password'];
         }
-        $user->update($validated);
-        return response()->json($user->load('role'));
+
+        try{
+            $updatedUser = $userToUpdate -> save();
+            if ($updatedUser){
+                return response ()->json($updatedUser);
+            }
+            else {
+                return "User not Updated";
+            }
+        }
+        catch (\Exception $e) {
+            return response()-> json ([
+                "Error"=>"Error creating a role"
+            ],500 );                             //If you see this error it means createRole() is failing
+        }
     }
+
+    //     if (isset($validated['password'])) {
+    //         $validated['password'] = Hash::make($validated['password']);
+    //     }
+    //     $user->update($validated);
+    //     return response()->json($user->load('role'));
+    // }
 
     public function destroy(User $user): JsonResponse
     {
@@ -90,3 +104,11 @@ class UserController extends Controller
         return response()->json(null, 204);
     }
 }
+
+
+        // if ($request->hasFile('user_photo')) {
+        //     $filename = $request->file('user_photo')->store('posts', 'public');
+        // } else {
+        //     $filename = Null;
+        // }
+        // $user->user_photo = $filename;
