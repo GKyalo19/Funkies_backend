2718339fa2793ed7fcf3eb708bc21361a38e6540
Grace Kyalo <kyalograce21@gmail.com>
2025-05-13 09:22:01 +0300

    MPesa ready

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -4,90 +4,97 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
 
 class MpesaController extends Controller
 {
-    public function generateAccessToken(){
-        $consumer_key = "GOsNjC8cyxtA6JM8B0zMMMG3cDE8FebsXvjG3nQeAtDc8fE8";
-        $consumer_secret = "RdzHHaIpTUqYbEoz5G8NFd1rhEAzGUK9ZxN0OnFPfAK1x9Tn6U9pZn7MhHpkItdp";
-        $credentials = base64_encode($consumer_key.":".$consumer_secret);
+    public function STKPush(Request $request)
+    {
+       date_default_timezone_set('Africa/Nairobi');
 
-        $url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+       $request -> validate([
+        'phone'=>'required|string',
+        'account_reference' => 'required|string',
+        'transaction_desc' => 'required|string'
+       ]);
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '. $credentials));
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            # access token
+            $consumerKey = 'K4RpCHURtlACH5yneIRcEp0KA1erkyNrnnVC7xLzCIqkNAMq'; //Fill with your app Consumer Key
+            $consumerSecret = 'J26kK0Iu45sf1F92UC2ghwkhZqfFCjsK9AbIsXRruLiCEftWyLHSZiyigeMUPYpi'; // Fill with your app Secret
 
-        $curl_response = curl_exec($curl);
+            // Dynamic values
+            $PartyA = $request -> input('phone'); // This is your phone number,
+            $AccountReference = $request -> input('account_reference');
+            $TransactionDesc = $request -> input('transaction_desc');
 
-        if (curl_errno($curl)) {
-            return response()->json(['error' => curl_error($curl)], 500);
+            //Static values
+            $BusinessShortCode = '174379';
+            $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+            $Amount = '1';
+
+            # Get the timestamp, format YYYYmmddhms -> 20181004151020
+            $Timestamp = date('YmdHis');
+
+            # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+            $Password = base64_encode($BusinessShortCode . $Passkey . $Timestamp);
+
+            # header for access token
+            $headers = ['Content-Type:application/json; charset=utf8'];
+
+            # M-PESA endpoint urls
+            $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+            $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+            # callback url
+            $CallBackURL = 'https://funkies254-backend.onrender.com/';
+
+            $curl = curl_init($access_token_url);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($curl, CURLOPT_HEADER, FALSE);
+            curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+            $result = curl_exec($curl);
+            $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            $result = json_decode($result);
+            $access_token = $result->access_token;
+            curl_close($curl);
+
+            # header for stk push
+            $stkheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token];
+
+            # initiating the transaction
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $initiate_url);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+
+            $curl_post_data = array(
+                //Fill in the request parameters with valid values
+                'BusinessShortCode' => $BusinessShortCode,
+                'Password' => $Password,
+                'Timestamp' => $Timestamp,
+                'TransactionType' => 'CustomerPayBillOnline',
+                'Amount' => $Amount,
+                'PartyA' => $PartyA,
+                'PartyB' => $BusinessShortCode,
+                'PhoneNumber' => $PartyA,
+                'CallBackURL' => $CallBackURL,
+                'AccountReference' => $AccountReference,
+                'TransactionDesc' => $TransactionDesc
+            );
+
+            $data_string = json_encode($curl_post_data);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+            $curl_response = curl_exec($curl);
+
+            if(curl_errno($curl)) {
+                echo 'Curl error: ' . curl_error($curl);
+            } else {
+                echo $curl_response;
+            }
+
+            print_r($curl_response);
+
+            echo $curl_response;
         }
-
-        $access_token = json_decode($curl_response);
-
-        if (!isset($access_token->access_token)) {
-            return response()->json(['error' => 'Access token not generated', 'response' => $curl_response], 500);
-        }
-
-        return $access_token->access_token;
-    }
-
-    public function STKPush(){
-        $access_token = $this->generateAccessToken();
-        dd("Access Token: ", $access_token);
-
-        $BusinessShortCode = 174379;
-        $passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
-        $timestamp = Carbon::now()->format('YmdHis');
-
-        $password = base64_encode($BusinessShortCode.$passkey.$timestamp);
-
-        $Amount = 1;
-        $PartyA = 254702925121;
-        $PartyB = 174379;
-
-        $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, [
-            'Content-Type:application/json',
-            'Authorization: Bearer '.$access_token
-        ]);
-
-        $curl_post_data = array(
-            'BusinessShortCode'=>$BusinessShortCode,
-            'Password'=>$password,
-            'Timestamp'=>$timestamp,
-            'TransactionType'=>'CustomerPayBillOnline',
-            'Amount'=>$Amount,
-            'PartyA'=>$PartyA,
-            'PartyB'=>$PartyB,
-            'PhoneNumber'=>$PartyA,
-            'CallBackURL'=>'https://funkies254-backend.onrender.com/callback',
-            'AccountReference'=>'Funkies 254',
-            'TransactionDesc'=>'Transaction successful!'
-        );
-
-        $data_string = json_encode($curl_post_data);
-
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-
-        $curl_response = curl_exec($curl);
-
-        if (curl_errno($curl)) {
-            return response()->json(['error' => curl_error($curl)], 500);
-        }
-        curl_close($curl);
-
-        return $curl_response;
-
-    }
 }
